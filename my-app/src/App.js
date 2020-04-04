1a6e45bd1440a75cf01d804b0d1bba391e757b52
goldstha97 <goldstha@ufl.edu>
2020-04-04 22:07:13 +0000

    Lab Done

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Elephant.png"/>
+        <p>
+          Instrumentation Lab
+        </p>
+        <a
+         
+        >
+         Harrison Goldstein
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

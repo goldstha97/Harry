276fb26a109ac602dbdca1d7f5dc920df215aaa9
goldstha97 <goldstha@ufl.edu>
2020-04-04 22:37:35 +0000

    lab done pt 2

---
+++ my-app/build/precache-manifest.e9849383427d4e0109f68a8dd722e357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ca2659137830ed3e9edd94ed43c488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6de434bc5f8bf9889572",
+    "url": "/static/css/main.f263bfb6.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de434bc5f8bf9889572",
+    "url": "/static/js/main.d2b872a8.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
